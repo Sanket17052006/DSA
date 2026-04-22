46a20b2a1419bd11346af3551093aeec9e2ea88f
Sanket Srivastava <sanket17052006@gmail.com>
2026-04-22 14:26:34 +0530

    LC-142

---
+++ LC-142.java
@@ -0,0 +1,20 @@
+// LC - 142 : https://leetcode.com/problems/linked-list-cycle-ii/
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow; 
+    }
+}
