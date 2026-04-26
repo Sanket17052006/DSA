7c6a60ebd3da3cbeef59c7d26e7ecc9fb700253e
Sanket Srivastava <sanket17052006@gmail.com>
2026-04-27 00:47:11 +0530

    LC-98

---
+++ LC-98.java
@@ -0,0 +1,40 @@
+// LC - 98 : https://leetcode.com/problems/validate-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> l=new ArrayList<>();
+        inOrder(root,l);
+        for(int i=0;i<l.size()-1;i++){
+            if(l.get(i)>=l.get(i+1)){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+    void inOrder(TreeNode root,List<Integer> l){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left,l);
+        l.add(root.val);
+        inOrder(root.right,l);
+    }
+
+}
