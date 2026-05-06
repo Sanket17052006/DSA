69579444b1056e52749a7a2deea4b548e295d521
Sanket Srivastava <sanket17052006@gmail.com>
2026-05-06 10:10:39 +0530

    LC-1861

---
+++ LC-1861.java
@@ -0,0 +1,27 @@
+// LC - 1861 : https://leetcode.com/problems/rotating-the-box/
+
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+        for (int i = 0; i < m; i++) {
+            int last = n-1;
+            for (int j = n-1; j >= 0; j--) {
+                if (boxGrid[i][j] == '#') {
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][last] = '#';
+                    last--;
+                } else if (boxGrid[i][j] == '*') {
+                    last = j - 1;
+                }
+            }
+        }
+        char[][] ans = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+        return ans;
+    }
+}
